d54af29cc80b3d14fb78894804f4f6d99249fc60
jsepucas <jsepucas@myuax.com>
2023-11-09 13:22:00 +0100

    RegistroDeMaterias2

--- 5_RegistroDeMaterias.cpp
+++ 5_RegistroDeMaterias.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
+using namespace std;
+class Estudiante {
+public:
+    Estudiante(string nombre) : nombre(nombre) {}
+
+    void registrar_materia(const string& materia) {
+        materias.push_back(materia);
+    }
+
+    void listar_materias() {
+        cout << nombre << " ha registrado las siguientes materias: ";
+        for (const string& materia : materias) {
+            cout << materia << ", ";
+        }
+        cout << endl;
+    }
+};
