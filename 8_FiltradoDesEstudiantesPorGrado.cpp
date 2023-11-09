1bf31e02c08fa129c8f8bb876e9e800cf91abf7e
jsepucas <jsepucas@myuax.com>
2023-11-09 13:40:36 +0100

    Filtradodeestudiantesporgrado1

---
+++ 8_FiltradoDesEstudiantesPorGrado.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Estudiante {
+public:
+    Estudiante(string nombre, int grado) : nombre(nombre), grado(grado) {}
+string obtenerNombre() const {
+        return nombre;
+    }
+    int obtenerGrado() const {
+        return grado;
+    }
+};
