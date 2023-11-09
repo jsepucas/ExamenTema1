2c6e016338388242d7ede0f9e86cddc41ac44672
jsepucas <jsepucas@myuax.com>
2023-11-09 13:37:25 +0100

    ManejoDeAusencias2

--- 7_ManejoDeAusencias.cpp
+++ 7_ManejoDeAusencias.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class RegistroAsist {
+public:
+    RegistroAsist(string fecha, string estado) : fecha(fecha), estado(estado) {}
+
+    void Enseñar_Asist() {
+        cout << "Fecha del dia: " << fecha << ", Estado de la asistencia: " << estado << endl;
+    }
+private:
+    string fecha;
+    string estado;
+};
+
