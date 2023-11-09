9f99c9ed9f39c7f046744ce2bbb7bf17fba12ada
jsepucas <jsepucas@myuax.com>
2023-11-09 13:13:53 +0100

    ManejoDeExcepciones2

--- 4_ManejoDeExcepciones.cpp
+++ 4_ManejoDeExcepciones.cpp
@@ -10,8 +10,20 @@ int divide(int x, int y) {
 
 int main() {
     int num, denom;
-    cout << "Introduzca el numerador: "
+    cout << "Introduzca el numerador: ";
     cin >> num;
-    cout << "Introduzca el denominador: "
+    cout << "Introduzca el denominador: ";
     cin >> denom;
+
+    try {
+        int resultado = divide(num, denom);
+        cout << "Resultado: " << resultado << endl;
+    } catch (const char* mensaje) {
+        cout << "Excepción capturada: " << mensaje << endl;
+    }
+        cout << "El programa continua aun habiendo ocurrido la excepciun." << endl;
+
+
+return 0;
+
 }
