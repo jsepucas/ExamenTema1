e2ef1a8902f3517b3e4e6c48749bd2b15999ceb7
jsepucas <jsepucas@myuax.com>
2023-11-09 13:42:12 +0100

    Filtradodeestudiantesporgrado3

--- 8_FiltradoDesEstudiantesPorGrado.cpp
+++ 8_FiltradoDesEstudiantesPorGrado.cpp
@@ -15,4 +15,32 @@ string obtenerNombre() const {
 private:
     string nombre;
     int grado;
+
 };
+
+int main() {
+vector<Estudiante> estudiantes;
+
+    estudiantes.push_back(Estudiante("Juan", 5));
+    estudiantes.push_back(Estudiante("Maria", 6));
+    estudiantes.push_back(Estudiante("Pedro", 5));
+    estudiantes.push_back(Estudiante("Luis", 4));
+    estudiantes.push_back(Estudiante("Ana", 6));
+
+    cout << "Grados disponibles:\n";
+    for (const Estudiante& estudiante : estudiantes) {
+        cout << estudiante.obtenerNombre() << " - Grado " << estudiante.obtenerGrado() << endl;
+    }
+
+    int gradoBuscado;
+    cout << "Elija un grado para filtrar: ";
+    cin >> gradoBuscado;
+
+    cout << "Estudiantes del grado " << gradoBuscado << ":\n";
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.obtenerGrado() == gradoBuscado) {
+            cout << estudiante.obtenerNombre() << endl;
+        }
+    }
+    return 0;
+}
